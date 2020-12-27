7971db2730da86692ecfbb971158371e04a7c4b4
manishsalian09 <manishsalian09@gmail.com>
2020-12-27 22:25:34 +0530

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ObjectRecognition/>
+</template>
+
+<script>
+import ObjectRecognition from './components/ObjectRecognition.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ObjectRecognition
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
