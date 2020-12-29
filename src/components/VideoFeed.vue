3486ad42628dcc766cd47c088f36291e04fd1949
manishsalian09 <manishsalian09@gmail.com>
2020-12-29 11:04:04 +0530

    drawing canvas around recognized objects

--- src/components/VideoFeed.vue
+++ src/components/VideoFeed.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
-    <button @click="start">Start</button>
-    <div id="camera">
-      <video ref="videoRef"></video>
-      <canvas ref="output"></canvas>
-    </div>
+  <div id="camera">
+    <video ref="videoRef" width="640" height="480"></video>
+    <canvas ref="output"></canvas>
   </div>
 </template>
 
@@ -12,7 +9,7 @@
 import { defineComponent, ref, onMounted, toRefs } from "vue";
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import '@tensorflow/tfjs-backend-webgl';
-import '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-cpu';
 
 export default defineComponent({
   name: "VideoFeed",
@@ -25,9 +22,10 @@ export default defineComponent({
   mounted() {
     this.videoRef = this.$refs.videoRef;
     this.canvasRef = this.$refs.output;
+    this.startVideoFeed();
   },
   methods: {
-    start() {
+    startVideoFeed() {
       navigator.mediaDevices.getUserMedia({ video: true, audio: false })
       .then(stream => {
         this.videoRef.srcObject = stream;
@@ -40,40 +38,27 @@ export default defineComponent({
     },
     async predictObjects() {
       const model = await cocoSsd.load();
-      const predictions = await model.detect(this.videoRef);
-      console.log('Predictions: ');
-      console.log(predictions);
-      this.draw(predictions);
+      setInterval(async () => {
+        const predictions = await model.detect(this.videoRef);
+        console.log(predictions);
+        this.draw(predictions);
+      }, 1000);
     },
     draw(predictions) {
       const ctx = this.canvasRef.getContext("2d");
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      const font = "24px helvetica";
-      ctx.font = font;
-      ctx.textBaseline = "top";
-
+      this.canvasRef.height = this.videoRef.height;
+      this.canvasRef.width = this.videoRef.width;
       predictions.forEach(prediction => {
-        const x = prediction.bbox[0];
-        const y = prediction.bbox[1];
-        const width = prediction.bbox[2];
-        const height = prediction.bbox[3];
-        // Draw the bounding box.
-        ctx.strokeStyle = "#2fff00";
-        ctx.lineWidth = 1;
-        ctx.strokeRect(x, y, width, height);
-        // Draw the label background.
-        ctx.fillStyle = "#2fff00";
-        const textWidth = ctx.measureText(prediction.class).width;
-        const textHeight = parseInt(font, 10);
-        // draw top left rectangle
-        ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
-        // draw bottom left rectangle
-        ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
-
-        // Draw the text last to ensure it's on top.
-        ctx.fillStyle = "#000000";
-        ctx.fillText(prediction.class, x, y);
-        ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+        const [x, y, width, height] = prediction['bbox']; 
+        const text = prediction['class']; 
+        ctx.strokeStyle = 'green'
+        ctx.font = '18px Arial';
+        ctx.fillStyle = 'green';
+        ctx.beginPath();   
+        ctx.fillText(text, x, y);
+        ctx.rect(x, y, width, height); 
+        ctx.stroke();
       });
     }
   }
@@ -82,4 +67,15 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+video {
+  left: 0;
+  right: 0;
+  position: absolute;
+}
+
+canvas {
+  left: 0;
+  right: 0;
+  position: absolute;
+}
 </style>
